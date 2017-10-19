b0434a29891206086beb1f0843a7d11b821ce14c
aikuakdi <1774104802@qq.com>
2017-10-19 15:06:45 +0800

    extract-files—2-one-fold

---
+++ ReadMe.txt
@@ -0,0 +1,16 @@
+将sourcefold文件夹下的所有文件放到targetfold文件夹中
+重复的文件名前面加前缀:copy_
+
+
+tuples=os.walk(fin_path) #返回生成器
+for tup in tuples:
+  print tup
+
+输出:
+('E:\\extract-files\\sourcefold', ['smallfold_1', 'smallfold_2', 'smallfold_3'], ['file.txt', 'file_1-1.txt'])
+('E:\\extract-files\\sourcefold\\smallfold_1', ['tinyfold_1-1'], ['file_1-1.txt', 'file_1-2.txt'])
+('E:\\extract-files\\sourcefold\\smallfold_1\\tinyfold_1-1', [], ['file_1-1-1.txt'])
+('E:\\extract-files\\sourcefold\\smallfold_2', [], ['file_1-1.txt', 'file_2-1.txt'])
+('E:\\extract-files\\sourcefold\\smallfold_3', [], [])
+
+注:tup[2]为tup[0]目录下咋文件列表
---
+++ extract_files.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import re
+import sys
+import os
+import shutil
+'''
+将sourcefold文件夹下的所有文件放到targetfold文件夹中
+重复的文件名前面加前缀:copy_
+'''
+fin_path = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'sourcefold')
+fout_path = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'targetfold')
+
+tuples=os.walk(fin_path) #返回生成器
+for tup in tuples:
+  #tup=(dirpath,dirnames,filename)
+  #dirpath:是一个字符串,该目录的路径;
+  #dirnames:是dirpath子目录的名称列表;
+  #filename:是dirpath中的非目录的名称列表.
+  # print tup
+  files=tup[2]
+  if files: #不为[]
+    for file in files:
+      sourcefile_path=os.path.join(tup[0], file)
+      targetfile_path=os.path.join(fout_path, file)
+      while True: #防止重名文件
+        if os.path.exists(targetfile_path): #判断文件是否已存在
+          head,tail=os.path.split(targetfile_path)
+          targetfile_path=os.path.join(head, 'copy_'+tail)
+        else:
+          break
+      shutil.copyfile(sourcefile_path,targetfile_path)
+      #注:若目标文件夹下已存在该文件名,则将会覆盖
